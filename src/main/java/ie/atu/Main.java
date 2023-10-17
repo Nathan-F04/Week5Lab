f8d5389cafe4ed170709b7d56cda8c2e13f49837
Nathan <G00413711@atu.ie>
2023-10-17 13:39:54 +0100

    Edited two instances of person in the main class and created new method displayinfo

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,9 +4,9 @@ package ie.atu;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Person first = new Person("Paul","Lennon",21);
-        System.out.println("You entered " + first.firstName + " " + first.lastName + " " + first.age + " ");
-        Person second = new Person();
-        System.out.println("You entered " + second.firstName + " " + second.lastName + " " + second.age + " ");
+        Person paulLen = new Person("Paul","Lennon",21);
+        System.out.println("You entered " + paulLen.firstName + " " + paulLen.lastName + " " + paulLen.age + " ");
+        Person johnJoe = new Person();
+        System.out.println("You entered " + johnJoe.firstName + " " + johnJoe.lastName + " " + johnJoe.age + " ");
         }
     }
